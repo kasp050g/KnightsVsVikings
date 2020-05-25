55b3eaed8680adc5822572056cb0d2f98c263792
whatever654 <2202asmund>
2020-05-26 00:31:23 +0200

    Update AsmundScene.cs

--- KnightsVsVikings/KnightsVsVikings/Script/TheGame/Scenes/Test Scenes/AsmundScene.cs
+++ KnightsVsVikings/KnightsVsVikings/Script/TheGame/Scenes/Test Scenes/AsmundScene.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MainSystemFramework;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 
 namespace KnightsVsVikings
 {
     public class AsmundScene : Scene
     {
+        Scene myScene;
         public override void Initialize()
         {
             base.Initialize();
+
+            GameObject background = new GameObject();
+            SpriteRenderer sr = new SpriteRenderer("Map", EOriginPosition.TopLeft, 0.01f);
+
+            ImageGUI image = new ImageGUI(sr, false, false);
+            background.AddComponent<SpriteRenderer>(sr);
+            background.AddComponent<ImageGUI>(image);
+
+            myScene.Instantiate(background);
         }
 
         public override void OnSwitchAwayFromThisScene()
