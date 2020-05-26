7ee1cea6cfe95777d424b45bbfed1b673f9a74e7
whatever654 <2202asmund>
2020-05-26 02:00:07 +0200

    New QuitGame buttons

--- KnightsVsVikings/KnightsVsVikings/Script/TheGame/Scenes/Test Scenes/AsmundScene.cs
+++ KnightsVsVikings/KnightsVsVikings/Script/TheGame/Scenes/Test Scenes/AsmundScene.cs
@@ -18,15 +18,31 @@ namespace KnightsVsVikings
             base.Initialize();
 
             GameObject background = new GameObject();
-            CSpriteRenderer sr = new CSpriteRenderer("Map", EOriginPosition.TopLeft, 0.01f);
-
-            GUIImage image = new GUIImage(sr, false, false);
-            background.AddComponent<CSpriteRenderer>(sr);
-            background.AddComponent<GUIImage>(image);
-
+            CSpriteRenderer backgroundSR = new CSpriteRenderer("Map", EOriginPosition.TopLeft, 0.01f);
+            GUIImage backgroundImage = new GUIImage(backgroundSR, false, false);
+            background.AddComponent<CSpriteRenderer>(backgroundSR);
+            background.AddComponent<GUIImage>(backgroundImage);
             background.Transform.Scale = GraphicsSetting.Instance.ScreenSize / new Vector2(2048, 1536); //2048, 1536 is the image's default size
 
-           Instantiate(background);
+            GameObject quitGame = new GameObject();
+            CSpriteRenderer quitGameSR = new CSpriteRenderer("QuitGame", EOriginPosition.TopLeft, 0.02f);
+            GUIImage quitGameImage = new GUIImage(quitGameSR, false, false);
+            quitGame.AddComponent<CSpriteRenderer>(quitGameSR);
+            quitGame.AddComponent<GUIImage>(quitGameImage);
+            quitGame.Transform.Scale = GraphicsSetting.Instance.ScreenSize / new Vector2(2222, 340); //2222, 340 is a tenth of the image's default size
+            quitGame.Transform.Position = new Vector2(GraphicsSetting.Instance.ScreenSize.X / 100, GraphicsSetting.Instance.ScreenSize.Y / 1.2f);
+
+            GameObject campaign = new GameObject();
+            CSpriteRenderer campaignSR = new CSpriteRenderer("QuitGame", EOriginPosition.TopLeft, 0.02f);
+            GUIImage campaignImage = new GUIImage(campaignSR, false, false);
+            campaign.AddComponent<CSpriteRenderer>(campaignSR);
+            campaign.AddComponent<GUIImage>(campaignImage);
+            campaign.Transform.Scale = GraphicsSetting.Instance.ScreenSize / new Vector2(2222, 340); //2222, 340 is a tenth of the image's default size
+            campaign.Transform.Position = new Vector2(GraphicsSetting.Instance.ScreenSize.X / 100, GraphicsSetting.Instance.ScreenSize.Y / 1.4f);
+
+            Instantiate(background);
+            Instantiate(quitGame);
+            Instantiate(campaign);
         }
 
         public override void OnSwitchAwayFromThisScene()
