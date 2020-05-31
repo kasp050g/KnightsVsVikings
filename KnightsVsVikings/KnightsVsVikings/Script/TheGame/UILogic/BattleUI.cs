fade9687bfcd564556638bdab9cddc49577dea84
whatever654 <2202asmund>
2020-05-31 22:17:08 +0200

    first draft of battleUI

--- KnightsVsVikings/KnightsVsVikings/Script/TheGame/UILogic/BattleUI.cs
+++ KnightsVsVikings/KnightsVsVikings/Script/TheGame/UILogic/BattleUI.cs
@@ -23,14 +23,23 @@ namespace KnightsVsVikings
             MouseSettings.Instance.IsMouseVisible(true);
 
             Texture2D texture1 = SpriteContainer.Instance.Sprite["ActionBar02"];
+            Texture2D gold = SpriteContainer.Instance.Sprite["Gold"];
+            Texture2D iron = SpriteContainer.Instance.Sprite["Iron"];
+            Texture2D wood = SpriteContainer.Instance.Sprite["Wood"];
+            Texture2D food = SpriteContainer.Instance.Sprite["Food"];
 
-            CreateBorder(new Vector2(300, 525), texture1, new Vector2(0.8f, 0.45f));
-            CreateBorder(new Vector2(0, 460), texture1, new Vector2(0.43f, 0.6f));
-            CreateBorder(new Vector2(980, 460), texture1, new Vector2(0.43f, 0.6f));
-            CreateBorder(new Vector2(855, 525), texture1, new Vector2(0.18f, 0.29f));
-            CreateBorder(new Vector2(855, 650), texture1, new Vector2(0.18f, 0.16f));
+            CreateBorder(new Vector2(300, 525), texture1, new Vector2(0.8f, 0.45f), 0.1f);
+            CreateBorder(new Vector2(0, 460), texture1, new Vector2(0.43f, 0.6f), 0.1f);
+            CreateBorder(new Vector2(980, 460), texture1, new Vector2(0.43f, 0.6f), 0.1f);
+            CreateBorder(new Vector2(855, 525), texture1, new Vector2(0.18f, 0.29f), 0.1f);
+            CreateBorder(new Vector2(855, 650), texture1, new Vector2(0.18f, 0.16f), 0.1f);
             
-            CreateBorder(new Vector2(-50, 0), texture1, new Vector2(2.0f, 0.1f));
+            CreateBorder(new Vector2(-50, 0), texture1, new Vector2(2.0f, 0.1f), 0.1f);
+            
+            CreateBorder(new Vector2(360, 0), gold, new Vector2(0.15f, 0.15f), 0.3f);
+            CreateBorder(new Vector2(560, 0), iron, new Vector2(0.15f, 0.15f), 0.3f);
+            CreateBorder(new Vector2(760, 0), wood, new Vector2(0.15f, 0.15f), 0.3f);
+            CreateBorder(new Vector2(960, 0), food, new Vector2(0.15f, 0.15f), 0.3f);
 
             CreateText("Dmg:", new Vector2(350, 550));
             CreateText("Arm:", new Vector2(350, 610));
@@ -44,13 +53,13 @@ namespace KnightsVsVikings
             CreateText("Portrait", new Vector2(870, 530));
             CreateText("Menu", new Vector2(880, 650));
             
-            CreateText("Resource 1:", new Vector2(400, 0));
-            CreateText("Resource 2:", new Vector2(600, 0));
-            CreateText("Resource 3:", new Vector2(800, 0));
-            CreateText("Resource 4:", new Vector2(1000, 0));
+            CreateText("0", new Vector2(400, 0));
+            CreateText("0", new Vector2(600, 0));
+            CreateText("0", new Vector2(800, 0));
+            CreateText("0", new Vector2(1000, 0));
         }
 
-        private void CreateBorder(Vector2 pos, Texture2D texture, Vector2 scale)
+        private void CreateBorder(Vector2 pos, Texture2D texture, Vector2 scale, float depth)
         {
             GameObject go = new GameObject();
             CSpriteRenderer iconSR = new CSpriteRenderer(texture);
@@ -60,7 +69,7 @@ namespace KnightsVsVikings
             go.Transform.Scale = scale;
             go.Transform.Position = pos;
             myScene.Instantiate(go);
-            iconSR.LayerDepth = 0.1f;
+            iconSR.LayerDepth = depth;
         }
         private void CreateText(string desc, Vector2 pos)
         {
